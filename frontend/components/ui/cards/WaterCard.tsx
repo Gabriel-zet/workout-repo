d71c1988ee593861fd3c0900064d922519a02b8d
gabrielbispoo <gabrielbispo20k@gmail.com>
2026-03-18 22:30:38 -0300

    Feat(home): make cards responsive for grid

--- frontend/components/ui/cards/WaterCard.tsx
+++ frontend/components/ui/cards/WaterCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, useWindowDimensions } from "react-native";
 import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function WaterCard() {
@@ -8,49 +8,48 @@ export default function WaterCard() {
   const filledBars = 7; // Representa o 1.5L de 2.5L
 
   return (
-    <View className="bg-[#131313] rounded-[48px] py-10 px-8 w-[340px] border border-neutral-900 shadow-xl">
-      
+    <View className="bg-[#111111] rounded-[36px] p-5 border-2 border-[#2A2A2A] w-full">
+
       {/* Cabeçalho */}
-      <View className="flex-row items-center mb-4">
+      <View className="flex-row items-center">
         {/* Usando o glass-whiskey do FA5 que lembra o copo quadrado do design */}
-        <FontAwesome5 name="glass-whiskey" size={20} color="#FFFFFF" />
-        <Text className="text-white text-2xl font-regular ml-3">
+        <FontAwesome5 name="glass-whiskey" size={12} color="#FFFFFF" />
+        <Text className="text-white text-sm font-regular ml-2.5">
           Água diária
         </Text>
       </View>
 
       {/* Conteúdo Principal (Valor + Meta) */}
-      <View className="flex-row items-center mb-6">
-        <Text className="text-[#FF6B00] text-[100px] leading-[110px] font-medium tracking-tighter mr-3">
+      <View className="flex-row items-center">
+        <Text className="text-[#FF6B00] text-[50px] font-medium mr-2.5">
           1.5
         </Text>
 
-        <View className="flex-col justify-center mt-2">
+        <View className="flex-col justify-center">
           {/* Badge de Aumento */}
-          <View className="bg-[#303E00] rounded-full px-3 py-1 flex-row items-center self-start mb-1">
-            <FontAwesome5 name="arrow-up" size={12} color="#A3E600" />
-            <Text className="text-[#A3E600] font-semibold ml-1 text-sm">
+          <View className="bg-[#303E00] rounded-full px-2.5 py-0.5 flex-row items-center self-start mb-1">
+            <FontAwesome5 name="arrow-up" size={10} color="#A3E600" />
+            <Text className="text-[#A3E600] font-medium ml-1 text-sm">
               900 ml
             </Text>
           </View>
-          
+
           {/* Valor Total */}
-          <Text className="text-white text-2xl font-medium">
+          <Text className="text-white text-md font-regular">
             /2.5 Litros
           </Text>
         </View>
       </View>
 
       {/* Barra de Progresso em Gomos */}
-      <View className="bg-[#262626] rounded-2xl py-4 px-4 flex-row justify-between mb-6">
+      <View className="bg-[#262626] rounded-xl py-2 px-3 flex-row justify-between mb-2">
         {Array.from({ length: totalBars }).map((_, index) => {
           const isFilled = index < filledBars;
           return (
             <View
               key={index}
-              className={`w-[10px] h-[36px] rounded-full ${
-                isFilled ? "bg-[#FF6B00]" : "bg-[#452615]"
-              }`}
+              className={`w-[5px] h-[26px] rounded-full ${isFilled ? "bg-[#FF6B00]" : "bg-[#452615]"
+                }`}
             />
           );
         })}
@@ -58,12 +57,12 @@ export default function WaterCard() {
 
       {/* Rodapé (Restante) */}
       <View className="flex-row items-center justify-center opacity-60">
-        <FontAwesome5 name="flag-checkered" size={16} color="#FFFFFF" />
-        <Text className="text-white text-lg font-regular ml-3">
+        <FontAwesome5 name="flag-checkered" size={12} color="#FFFFFF" />
+        <Text className="text-white text-sm font-regular ml-2.5">
           1 litro até meta
         </Text>
       </View>
-      
+
     </View>
   );
 }
